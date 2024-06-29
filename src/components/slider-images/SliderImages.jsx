d5add584751d7b6b043384303706e4e423c0d10d
Mohammadreza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-06-29 22:44:16 +0330

    Development slider images

--- src/components/slider-images/SliderImages.jsx
+++ src/components/slider-images/SliderImages.jsx
@@ -4,17 +4,42 @@ import {Swiper, SwiperSlide} from "swiper/react";
 function SliderImages() {
     // JSX
     return (
-        <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-        </Swiper>
+        <div className="pt-[65px]">
+
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+            >
+                {/* Slides */}
+                <SwiperSlide className="">
+                    <img src="https://digital.masirwp.com/wp-content/uploads/2023/09/64e1ebd4426e5.jpg"
+                         alt="موبایل هونور"
+                         loading="lazy"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://digital.masirwp.com/wp-content/uploads/2023/09/64dcbd80ecac5.jpg"
+                         alt="موبایل نوکیا"
+                         loading="lazy"/>
+
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://digital.masirwp.com/wp-content/uploads/2023/09/full-screen-banners.jpg"
+                         alt="ساعت هوشمند"
+                         loading="lazy"/>
+
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://digital.masirwp.com/wp-content/uploads/2023/09/airpod.jpg"
+                         alt="محبوب ترین ایرپاد ها"
+                         loading="lazy"/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src="https://digital.masirwp.com/wp-content/uploads/2023/09/mobile-banner.jpg"
+                         alt="موبایل سامسونگ"
+                         loading="lazy"/>
+                </SwiperSlide>
+            </Swiper>
+        </div>
     );
 }
 
